99540c9c337d955502248d54d2170e906172fee1
Ewerton Igor <ewertonigor@icloud.com>
2023-06-26 18:31:48 -0300

    Teste com spyOn

--- src/services/persistency.test.ts
+++ src/services/persistency.test.ts
@@ -1,8 +1,26 @@
-it('Descrição do teste (IT)', () => {
-  const number = 1;
-  expect(number).toBe(1);
-});
-test('Descrição do teste (TESTE)', () => {
-  const nome = 'Igor';
-  expect(nome).toBe('Igor');
+import { Persistency } from './persistency';
+
+// Padrão de testes
+describe('Persistency', () => {
+  afterEach(() => jest.clearAllMocks()); // Depois de cada teste o jest é limpado
+
+  test('should return undefined', () => {
+    // System under test
+    const sut = new Persistency(); // sut -> Classe que está sendo testada
+    expect(sut.saveOrder()).toBeUndefined();
+  });
+  test('should call console.log once', () => {
+    // System under test
+    const sut = new Persistency(); // sut -> Classe que está sendo testada
+    const consoleSpy = jest.spyOn(console, 'log'); // O jest vai ficar observando o console
+    sut.saveOrder(); // Chamando o metodo saveOrder.
+    expect(consoleSpy).toHaveBeenCalledTimes(1); // Vai ser chamado o console uma vez?
+  });
+  test('should call console.log with "Pedido salvo com sucesso..."', () => {
+    // System under test
+    const sut = new Persistency(); // sut -> Classe que está sendo testada
+    const consoleSpy = jest.spyOn(console, 'log'); // O jest vai ficar observando o console
+    sut.saveOrder(); // Chamando o metodo saveOrder.
+    expect(consoleSpy).toHaveBeenCalledWith('Pedido salvo com sucesso...'); // Vai ser chamado o console uma vez?
+  });
 });
