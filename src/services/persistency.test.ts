507296c0cffbe347a376b440efd44dac2d8adf05
Ewerton Igor <ewertonigor@icloud.com>
2023-06-26 13:10:58 -0300

    Introdução ao jest

---
+++ src/services/persistency.test.ts
@@ -0,0 +1,8 @@
+it('Descrição do teste (IT)', () => {
+  const number = 1;
+  expect(number).toBe(1);
+});
+test('Descrição do teste (TESTE)', () => {
+  const nome = 'Igor';
+  expect(nome).toBe('Igor');
+});
