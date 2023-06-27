72bba926819b4a58d526ca8ca8f616cd10226193
Ewerton Igor <ewertonigor@icloud.com>
2023-06-27 12:58:37 -0300

    Testando product.ts

---
+++ src/class/product.test.ts
@@ -0,0 +1,17 @@
+import { Product } from './product';
+
+const createSut = (name: string, price: number): Product => {
+  return new Product(name, price);
+};
+
+// Padrão de testes
+describe('Product', () => {
+  afterEach(() => jest.clearAllMocks()); // Depois de cada teste o jest é limpado
+
+  test('should return undefined', () => {
+    // System under test
+    const sut = createSut('Camiseta', 40.99); // sut -> Classe que está sendo testada
+    expect(sut).toHaveProperty('name', sut.name);
+    expect(sut.price).toBeCloseTo(40.99); // Recomendado testar pontos flutuantes com 'toBeCloseTo'
+  });
+});
