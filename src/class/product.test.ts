307809a7f1f811c942d57bcaf0a6589c38c28165
Ewerton Igor <ewertonigor@icloud.com>
2023-06-27 12:59:48 -0300

    Corrigindo test

--- src/class/product.test.ts
+++ src/class/product.test.ts
@@ -8,7 +8,7 @@ const createSut = (name: string, price: number): Product => {
 describe('Product', () => {
   afterEach(() => jest.clearAllMocks()); // Depois de cada teste o jest é limpado
 
-  test('should return undefined', () => {
+  test('should have properties name and price', () => {
     // System under test
     const sut = createSut('Camiseta', 40.99); // sut -> Classe que está sendo testada
     expect(sut).toHaveProperty('name', sut.name);
